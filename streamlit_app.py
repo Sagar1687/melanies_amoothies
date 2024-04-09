298db6cef29265068b2eadfd67dfacb2a10ac0ae
Sagar1687 <166543838+Sagar1687@users.noreply.github.com>
2024-04-10 00:49:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 #Convert snowflake dataframe to Pandas dataframe so we can use the LOC function
-pd_df=my_dataframe.pd.to_pandas()
+pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
